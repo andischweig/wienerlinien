13760c37c241ba78ec7fec01506d847110c0a671
andischweig <schweiger.andreas1994@gmail.com>
2022-03-03 14:52:23 +0100

    Remove loop=self.loop from async_timeout
    Since Home Assistant 2022.3 loop=self.loop is not allowed anymore.

--- custom_components/wienerlinien/sensor.py
+++ custom_components/wienerlinien/sensor.py
@@ -137,7 +137,7 @@ class WienerlinienAPI:
         value = None
         url = BASE_URL.format(self.stopid)
         try:
-            async with async_timeout.timeout(10, loop=self.loop):
+            async with async_timeout.timeout(10):
                 response = await self.session.get(url)
                 value = await response.json()
         except Exception:
